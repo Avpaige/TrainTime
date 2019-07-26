12187ace9572f9c87e8e03bd2974eb44b4a40ccd
Ari Virgil-Paige <avpaige@gmail.com>
2019-07-25 21:26:48 -0700

    css updates

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,15 +1,16 @@
-  // Your web app's Firebase configuration
-  var config = {
-    apiKey: "AIzaSyBW5rbUSAaSKVZ3hZedUft_OeocOPK3STE",
-    authDomain: "payroll-week-7.firebaseapp.com",
-    databaseURL: "https://payroll-week-7.firebaseio.com",
-    projectId: "payroll-week-7",
-    storageBucket: "",
-    messagingSenderId: "877587004908",
-    appId: "1:877587004908:web:5a5d83c5f36791c9"
-  };
-  // Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBuLsWT0Y8RVu6doFxFeVj9tNmgQfqbFbY",
+  authDomain: "week7hw2019.firebaseapp.com",
+  databaseURL: "https://week7hw2019.firebaseio.com",
+  projectId: "week7hw2019",
+  storageBucket: "",
+  messagingSenderId: "677063025467",
+  appId: "1:677063025467:web:bf8feb04246d3c94"
+};
+// Initialize Firebase
   firebase.initializeApp(config);
+  // Your web app's Firebase configuration
+
   var dataRef = firebase.database();
 
   var trainName = "";
@@ -19,18 +20,30 @@
   var nextArrival = 0;
   var minAway = 0;
 
-  $("#click").on("click", function (event){
+  $("#add").on("click", function (event){
     event.preventDefault();
-        trainName= $("#train").val().trim();
-        destination= $("#place").val().trim();
-        firstTrain= $("#traintime").val().trim();
-        frequency= $("#freq").val().trim();
 
-        dataRef.ref().push({
-            trainName: trainName,
-            destination: destination,
-            firstTrain: firstTrain,
-            frequency:frequency,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP,
-          });
+  
+        trainName = $("#train").val().trim();
+        destination= $("#place").val().trim();
+        firstTrain = $("#traintime").val().trim();
+        frequency = $("#freq").val().trim();
+
+        var newTrain ={
+          trainName: trainName,
+          destination: destination,
+          firstTrain: firstTrain,
+          frequency:frequency,
+          dateAdded: firebase.database.ServerValue.TIMESTAMP,
+        }
+
+        dataRef.ref().push(newTrain);
+
+          console.log(newTrain.trainName);
+          console.log(newTrain.destination);
+          console.log(newTrain.firstTrain);
+          console.log(newTrain.frequency);
+
+          alert("New Train Added");
   });
+
