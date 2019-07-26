e9374a2fa6f5a56f0ed2df446d1a2dfc4d660eca
Ari Virgil-Paige <avpaige@gmail.com>
2019-07-25 22:31:41 -0700

    working firebase

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 var config = {
   apiKey: "AIzaSyBuLsWT0Y8RVu6doFxFeVj9tNmgQfqbFbY",
   authDomain: "week7hw2019.firebaseapp.com",
@@ -7,26 +9,24 @@ var config = {
   messagingSenderId: "677063025467",
   appId: "1:677063025467:web:bf8feb04246d3c94"
 };
+
 // Initialize Firebase
   firebase.initializeApp(config);
+  console.log("firebase is " + firebase)
   // Your web app's Firebase configuration
 
   var database = firebase.database();
 
-  var trainName = "";
-  var destination = "";
-  var frequency = 0;
-  var firstTrain = 0;
-  var nextArrival = 0;
-  var minAway = 0;
 
   $("#add").on("click", function (event){
     event.preventDefault();
   
        var trainName = $("#train").val().trim();
        var destination= $("#place").val().trim();
-       var firstTrain = $("#traintime").val().trim();
+       var firstTrain =  $("#traintime").val().trim();
        var frequency = $("#freq").val().trim();
+
+       console.log("train name is " + trainName);
 
         var newTrain ={
           trainName: trainName,
@@ -34,15 +34,36 @@ var config = {
           firstTrain: firstTrain,
           frequency:frequency,
           dateAdded: firebase.database.ServerValue.TIMESTAMP,
-        }
+        };
 
         database.ref().push(newTrain);
 
-          console.log(newTrain.trainName);
-          console.log(newTrain.destination);
-          console.log(newTrain.firstTrain);
-          console.log(newTrain.frequency);
+        $("#train").val("");
+        $("#place").val("");
+        $("#rtraintime").val("");
+        $("#freq").val("");
 
-          alert("New Train Added");
+       alert("New Train Added");
   });
 
+  database.ref().on("child_added", function(snap) {
+    console.log("snap val is "+ snap.val());
+
+    var trainName = snap.val().trainName;
+    var destination= snap.val().destination;
+    var frequency =  snap.val().frequency;
+    var nextArrival = 0;
+    var minAway = 0;
+
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(destination),
+      $("<td>").text(frequency),
+      $("<td>").text(nextArrival),
+      $("<td>").text(minAway),
+      );
+
+    $(".table > tbody").append(newRow);
+
+  });
+});
